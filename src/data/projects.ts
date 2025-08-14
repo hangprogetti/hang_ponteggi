ca266a23fb34c25efe49875f1e95c1b4f7e7ae70
loren <lorenzoleaf@gmail.com>
2025-08-14 17:59:10 +0200

    Update project video links and add new project entries in projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -21,7 +21,7 @@ Un intervento pensato per valorizzare e preservare uno dei capolavori storici de
     }
   },
   {
-    video: "/assets/video/ponte_genova.mp4",
+    video: "/assets/video/ponte.mp4",
     title: "Ponte di Doccio, Valsesia",
     projectImage: "https://res.cloudinary.com/dczzzutnf/image/upload/v1752513207/ponte_doccio_xx6uiy.webp",
     shortDescription: `Hang ha realizzato una struttura che permette di lavorare su tutto il ponte, in condizioni di massima sicurezza ed affidabilità...`,
@@ -41,7 +41,7 @@ Un intervento pensato per valorizzare e preservare uno dei capolavori storici de
     }
   },
   {
-    video: "/assets/video/ponte.mp4",
+    video: "/assets/video/pont.mp4",
     title: "Ponteggio sospeso per far risplendere il ponte di ferro a Sesto Calende (VA)",
     projectImage: "https://res.cloudinary.com/dczzzutnf/image/upload/v1752277486/ponte_calende_vinqql.webp",
     shortDescription: `Sotto l’impalcato e sui fianchi, Hang ha provveduto all’installazione di un ponteggio sospeso per un intervento di manutenzione straordinaria...`,
@@ -59,7 +59,87 @@ Un intervento pensato per valorizzare e preservare uno dei capolavori storici de
       intervento: "Installazione ponteggi sospesi",
       area: "3000 m²"
     }
-  }
+  },
+  {
+    video: "/assets/video/pont.mp4",
+    title: "Cattedrale di Santa Maria Assunta Aosta",
+    projectImage: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753693651/cattedrale_mappa_rywbep.webp",
+    shortDescription: `L\'intervento ha richiesto una progettazione su misura, capace di garantire la massima stabilità senza compromettere l\'integrità architettonica e artistica dell’edificio...`,
+    description: "L\'intervento ha richiesto una progettazione su misura, capace di garantire la massima stabilità senza compromettere l\'integrità architettonica e artistica dell’edificio. Il ponteggio è stato installato con tecnologie ad incastro non invasive e strutture modulari che hanno permesso agli operatori di lavorare in sicurezza anche in punti di difficile accesso. Particolare attenzione è stata posta alla tutela delle superfici decorate e al rispetto delle funzioni liturgiche, assicurando la piena fruibilità degli spazi durante tutto il periodo dei lavori.",
+    images: [
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186113/aostacatedrale_xdpkcv.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186113/aostacatedrale_3_tka09f.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186113/aostacatedrale_4_jyq1q0.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186113/aostacatedrale_6_f0bjrk.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186113/aostacatedrale_8_f00ubl.avif"
+    ],
+    details: {
+      dove: "Aosta",
+      durata: "11 mesi",
+      intervento: "Installazione ponteggi sospesi",
+      area: "2500 m²"
+    }
+  },
+  {
+    video: "/assets/video/pont.mp4",
+    title: "Ponte a Colle Salvetti, Livorno",
+    projectImage: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753695607/Progetto_Ponte_ColleSalvetti_a00ivj.webp",
+    shortDescription: `Il ponteggio sospeso è stato dotato di piani di lavoro continui, parapetti di sicurezza e schermature laterali per la protezione ambientale e il contenimento dei materiali di cantiere...`,
+    description: "Il ponteggio sospeso è stato dotato di piani di lavoro continui, parapetti di sicurezza e schermature laterali per la protezione ambientale e il contenimento dei materiali di cantiere. Grazie all’uso di componenti modulari e leggere, Hang ha garantito un montaggio rapido e sicuro, ottimizzando i tempi di intervento e riducendo l’impatto sul contesto naturale circostante.",
+    images: [
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754418666/COLLE_SALVETTI_SITO-2_twllj9.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754418707/COLLE_SALVETTI_SITO-4_jm4nec.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754418723/COLLE_SALVETTI_SITO-10_wr1sot.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754418745/COLLE_SALVETTI_SITO-18_cjnqnq.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754418775/COLLE_SALVETTI_SITO-25_apa0wu.avif"
+    ],
+    details: {
+      dove: "Livorno",
+      durata: "10 mesi",
+      intervento: "Installazione ponteggi sospesi",
+      area: "3000 m²"
+    }
+  },
+  {
+    video: "/assets/video/pont.mp4",
+    title: "Torre ponteggio a Pieve Emanuele (MI)",
+    projectImage: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753710855/pieveEmnuele_hfane8.webp",
+    shortDescription: `Hang in questo cantiere ha adottato soluzioni ad alto standard di sicurezza, con parapetti, passerelle antiscivolo e dispositivi di protezione collettiva e una torre alta 50 metri...`,
+    description: "Hang in questo cantiere ha adottato soluzioni ad alto standard di sicurezza, con parapetti, passerelle antiscivolo e dispositivi di protezione collettiva e una torre alta 50 metri, assicurando una gestione efficiente del cantiere e il pieno rispetto delle normative vigenti.",
+    images: [
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754419062/PIEVE_EMANUELE_SITO-19_wxc7rk.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754419081/PIEVE_EMANUELE_SITO-24_qfy2bl.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754419102/PIEVE_EMANUELE_SITO-28_gdu3ak.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754419115/PIEVE_EMANUELE_SITO-32_xpvsty.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1754419140/PIEVE_EMANUELE_SITO-38_sn2wwj.avif"
+    ],
+    details: {
+      dove: "Pieve Emanuele (MI)",
+      durata: "6 mesi",
+      intervento: "Installazione ponteggi sospesi",
+      area: "1500 m²"
+    }
+  },
+  {
+    video: "/assets/video/pont.mp4",
+    title: "Ponte a Rapallo, Genova",
+    projectImage: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753711300/ponte_rapallo_j9hxnp.webp",
+    shortDescription: `Hang ha eseguito l’allestimento di un ponteggio sospeso su ponte viabilistico, nell’ambito di un intervento di manutenzione straordinaria dell’infrastruttura...`,
+    description: "Hang ha eseguito l’allestimento di un ponteggio sospeso su ponte viabilistico, nell’ambito di un intervento di manutenzione straordinaria dell’infrastruttura. Il ponteggio è stato ancorato alla parte inferiore dell’impalcato mediante sistemi certificati che non richiedono forature invasive, consentendo l’accesso sicuro agli operatori per lavorazioni su travi portanti, giunti e superfici ammalorate.",
+    images: [
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186598/rapallo_1_nfnwhj.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186600/rapallo_2_xb8h15.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186598/rapallo_3_ac7gxn.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186599/rapallo_4_xdcrmw.avif",
+      "https://res.cloudinary.com/dczzzutnf/image/upload/v1755186602/rapallo_5_gv9xec.avif"
+    ],
+    details: {
+      dove: "Rapallo, Genova",
+      durata: "8 mesi",
+      intervento: "Installazione ponteggi sospesi",
+      area: "2000 m²"
+    }
+  },
 ];
 
 export const mapProjects = [
@@ -91,39 +171,39 @@ export const mapProjects = [
     id: 3, 
     position: [45.73840293002966, 7.317805039468387] as [number, number], 
     name: "Cattedrale di Santa Maria Assunta Aosta", 
-    description: 'L\'intervento ha richiesto una progettazione su misura, capace di garantire la massima stabilità senza compromettere l\'integrità architettonica e artistica dell’edificio. Il ponteggio è stato installato con tecnologie ad incastro non invasive e strutture modulari che hanno permesso agli operatori di lavorare in sicurezza anche in punti di difficile accesso. Particolare attenzione è stata posta alla tutela delle superfici decorate e al rispetto delle funzioni liturgiche, assicurando la piena fruibilità degli spazi durante tutto il periodo dei lavori...', 
+    description: 'L\'intervento ha richiesto una progettazione su misura, capace di garantire la massima stabilità senza compromettere l\'integrità architettonica e artistica dell’edificio...', 
     imageUrl: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753693651/cattedrale_mappa_rywbep.webp",
-    showDiscoverLink: false
+    showDiscoverLink: true
   },
   { 
     id: 4, 
     position: [43.57064506536163, 10.426879468150057] as [number, number], 
     name: "Ponte a Colle Salvetti, Livorno", 
-    description: 'Il ponteggio sospeso è stato dotato di piani di lavoro continui, parapetti di sicurezza e schermature laterali per la protezione ambientale e il contenimento dei materiali di cantiere. Grazie all’uso di componenti modulari e leggere, Hang ha garantito un montaggio rapido e sicuro, ottimizzando i tempi di intervento e riducendo l’impatto sul contesto naturale circostante...', 
+    description: 'Il ponteggio sospeso è stato dotato di piani di lavoro continui, parapetti di sicurezza e schermature laterali per la protezione ambientale e il contenimento dei materiali di cantiere...', 
     imageUrl: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753695607/Progetto_Ponte_ColleSalvetti_a00ivj.webp",
-    showDiscoverLink: false
+    showDiscoverLink: true
   },
   { 
     id: 5, 
     position: [45.34308926727647, 9.196780424491662] as [number, number], 
-    name: "Pieve Emanuele (MI)", 
-    description: 'Hang in questo cantiere ha adottato soluzioni ad alto standard di sicurezza, con parapetti, passerelle antiscivolo e dispositivi di protezione collettiva e una torre alta 50 metri, assicurando una gestione efficiente del cantiere e il pieno rispetto delle normative vigenti...', 
+    name: "Torre ponteggio a Pieve Emanuele (MI)", 
+    description: 'Hang in questo cantiere ha adottato soluzioni ad alto standard di sicurezza, con parapetti, passerelle antiscivolo e dispositivi di protezione collettiva e una torre alta 50 metri...', 
     imageUrl: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753710855/pieveEmnuele_hfane8.webp",
-    showDiscoverLink: false
+    showDiscoverLink: true
   },
   { 
     id: 6, 
     position: [44.357822470687715, 9.223891811789565] as [number, number], 
     name: "Ponte a Rapallo, Genova", 
-    description: 'Hang ha eseguito l’allestimento di un ponteggio sospeso su ponte viabilistico, nell’ambito di un intervento di manutenzione straordinaria dell’infrastruttura. Il ponteggio è stato ancorato alla parte inferiore dell’impalcato mediante sistemi certificati che non richiedono forature invasive, consentendo l’accesso sicuro agli operatori per lavorazioni su travi portanti, giunti e superfici ammalorate...', 
+    description: 'Hang ha eseguito l’allestimento di un ponteggio sospeso su ponte viabilistico, nell’ambito di un intervento di manutenzione straordinaria dell’infrastruttura...', 
     imageUrl: "https://res.cloudinary.com/dczzzutnf/image/upload/v1753711300/ponte_rapallo_j9hxnp.webp",
-    showDiscoverLink: false
+    showDiscoverLink: true
   },
    { 
     id: 7, 
     position: [44.43058259297653, 8.789655983590677] as [number, number], 
     name: "Viadotto San Pietro, Genova", 
-    description: 'Hang ha realizzato una passerella multidirezionale, completa di torre scala, per consentire in sicurezza gli interventi sul cordolo est. La struttura, progettata su misura, ha garantito un accesso agevole e stabile agli operatori, permettendo lo svolgimento delle lavorazioni in quota nel pieno rispetto delle normative di sicurezza e senza interferenze con le aree sottostanti...', 
+    description: 'Hang ha realizzato una passerella multidirezionale, completa di torre scala, per consentire in sicurezza gli interventi sul cordolo est...', 
     imageUrl: "",
     showDiscoverLink: false
   },
